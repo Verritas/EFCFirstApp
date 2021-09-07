14f70ad7edb822844944a4a803afe783d9e91df1
Verritas <example@example.com>
2021-09-07 17:37:04 +0300

    Manipulations with data

--- EFCFirstApp/LinqExample.cs
+++ EFCFirstApp/LinqExample.cs
@@ -30,13 +30,83 @@ namespace EFCFirstApp
 
             using (linqExampledbContext db = new linqExampledbContext())
             {
-                var users = await db.Users
-                                    .Include(p => p.Company)
-                                    .Where(p => p.CompanyId == 1)
-                                    .ToListAsync();
+                var users = db.Users.Where(p => EF.Functions.Like(p.Name, "%Tom%"));
+                foreach (User_Linq user in users)
+                {
+                    Console.WriteLine($"{user.Name} ({user.Age})");
+                }
+
+                users = db.Users.OrderBy(p => p.Name);
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Id}.{user.Name} ({user.Age})");
+                }
+            }
+
+            using (linqExampledbContext db = new linqExampledbContext())
+            {
+                var users = db.Users.Join(db.Companies,
+                    u => u.CompanyId,
+                    c => c.Id,
+                    (u, c) => new
+                    {
+                        Name = u.Name,
+                        Company = c.Name,
+                        Age = u.Age
+                    });
+
+                foreach (var u in users)
+                {
+                    Console.WriteLine($"{u.Name} ({u.Company}) - {u.Age}");
+                }
+            }
+
+            using (linqExampledbContext db = new linqExampledbContext())
+            {
+                var groups = from u in db.Users
+                             group u by u.Company.Name into g
+                             select new
+                             {
+                                 g.Key,
+                                 Count = g.Count()
+                             };
+
+                foreach (var group in groups)
+                {
+                    Console.WriteLine($"{group.Key} - {group.Count}");
+                }
+            }
+
+            using (linqExampledbContext db = new linqExampledbContext())
+            {
+                var users = db.Users.Where(u => u.Age < 30)
+                    .Union(db.Users.Where(u => u.Name.Contains("Tom")));
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+
+            using (linqExampledbContext db = new linqExampledbContext())
+            {
+                var users = db.Users.Where(u => u.Age > 30)
+                    .Intersect(db.Users.Where(u => u.Name.Contains("Tom")));
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+
+            using (linqExampledbContext db = new linqExampledbContext())
+            {
+                var selector1 = db.Users.Where(u => u.Age > 30);
+                var selector2 = db.Users.Where(u => u.Name.Contains("Tom"));
+                var users = selector1.Except(selector2);
 
                 foreach (var user in users)
-                    Console.WriteLine($"{user.Name} ({user.Age}) - {user.Company.Name}");
+                {
+                    Console.WriteLine(user.Name);
+                }
             }
         }
     }
